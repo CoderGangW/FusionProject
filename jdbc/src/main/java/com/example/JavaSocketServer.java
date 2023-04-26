4fc46b180394c9a8c43d14b2c66f2c4c57686456
coderGangW <90552284+coderGangW@users.noreply.github.com>
2023-04-26 20:57:03 +0900

    주석

--- jdbc/src/main/java/com/example/JavaSocketServer.java
+++ jdbc/src/main/java/com/example/JavaSocketServer.java
@@ -17,9 +17,8 @@ public class JavaSocketServer {
    
     static DAO dao = new DAO();
 
+    // Socket server Open
     public void start(int port) throws IOException {
-
-        // Socket server Open
         serverSocket = new ServerSocket(port);
         System.out.println("\n√  JAVA 서버가 " + port +"번 포트에서 실행중입니다.");
         clientSocket = serverSocket.accept();
